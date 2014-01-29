aa4d6b90b0a882610d2a7c678ffa58151d391761
Himel Nag Rana <himel@loosemonkies.com>
2014-01-29 08:34:37 +0600

    testing with taking user input

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,36 +3,33 @@ require 'selenium-webdriver'
 class HomeController < ApplicationController
   def index
 
-    #Firefox browser instantiation
-    driver = Selenium::WebDriver.for :firefox
+    if params[:loginTest].present?
 
-    #Loading the Qubole URL
-    driver.navigate.to "http://www.facebook.com"
+      driver = Selenium::WebDriver.for :firefox
+      driver.navigate.to params[:loginTest][:Site_URL]
 
-    #User name input field identification and data entered
-    usernametext = driver.find_element(:id, 'email')
-    usernametext.send_keys "providerlm@gmail.com" #put your actual username
+      usernametext = driver.find_element(:id, params[:loginTest][:username_Field_Id])
+      usernametext.send_keys params[:loginTest][:username]
 
-    #Password input field identification and data entered
-    passwordtext = driver.find_element(:id, 'pass')
-    passwordtext.send_keys "123456" #put your actual password
+      passwordtext = driver.find_element(:id, params[:loginTest][:password_Field_Id])
+      passwordtext.send_keys params[:loginTest][:password]
 
-    #Sign in button identification and click it
-    signinbutton = driver.find_element(:id, 'u_0_o')
-    signinbutton.click
+      signinbutton = driver.find_element(:id, params[:loginTest][:login_Button_Id])
+      signinbutton.click
 
-    #Click Kousik to view Drop-down Menu
-    wait = Selenium::WebDriver::Wait.new(:timeout => 20) # seconds
-    wait.until { element = driver.find_element(:link_text, 'Kousik')
-      element.click
-    }
+      wait = Selenium::WebDriver::Wait.new(:timeout => 20)
+      wait.until { element = driver.find_element(:link_text, params[:loginTest][:logout_Menu_Text])
+        element.click
+      }
 
-    #Identify signout link and click on it
-    element1 = driver.find_element(:link_text, 'Sign Out')
-    element1.click
-    puts "Using valid data login and logout functionality compleated Successfully!"
+      element1 = driver.find_element(:link_text, 'Sign Out')
+      element1.click
+      puts "Using valid data login and logout functionality compleated Successfully!"
 
-    #Quitting the browser
-    driver.quit
+      driver.quit
+
+      abort('test successful')
+    end
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Tester::Application.routes.draw do
   # You can have the root of your site routed with "root"
    root 'home#index'
 
+   post '/home/index' => 'home#index'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
